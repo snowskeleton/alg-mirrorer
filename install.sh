914f9fabae20a7198fe427cb7882f36f0bfd687e
Isaac Lyons <isaac@snowskeleton.net>
2022-05-04 15:03:22 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ LIB="/usr/lib/mir"
 #sudo mkdir ${LIB}
 
 #uncomment to remove previous installation
-#sudo rm -rf ${LIB} ${DEST}
+sudo rm -rf ${LIB} ${DEST}
 
 #comment out to not install (acts as a complete uninstall if paired with the above)
-sudo cp -r ../algMirrorer ${LIB}
+sudo cp -r ../algMirrorer ${LIB} 2>/dev/null
 sudo ln -s ${LIB}/main.py ${DEST}
 sudo chmod 755 ${DEST}
